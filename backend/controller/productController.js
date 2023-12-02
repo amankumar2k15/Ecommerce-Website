e460fbbaace0c4e7192275b5b1c5fcdbae8dae8a
aman.kumar2k15@gmail.com <aman.kumar2k15@gmail.com>
2023-12-02 13:11:20 +0530

    changes in backend

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -67,3 +67,92 @@ const getAllProduct = async (req, res) => {
 }
 
 module.exports = { createProduct, getAllProduct } 
+
+
+// search API
+// router.get("/search/:s", async (req, res) => {
+//     const s = req.params.s;
+//     if (!s) {
+//       return res.status(400).json("not found")
+//     }
+//     try {
+//       const products = await Product.find(
+//         {
+//           $or: [
+//             { "title": { $regex: s, $options: "i" } },
+//             { "productno": { $regex: s, $options: "i" } },
+//             { "desc": { $regex: s, $options: "i" } },
+//             { "categories": { $in: [s] } }
+//           ]
+//         },
+//         {
+//           title: 1,
+//           _id: 1
+//         }
+//       ).limit(5)
+  
+//       return res.status(200).json(products)
+//     } catch (error) {
+//       console.log(error)
+//       return res.status(500).json("internal server error")
+//     }
+//   })
+
+
+// API for multiple things - pagination , category, color , size
+// router.get("/allinfo", async (req, res) => {
+//     const { page = 1, limit = 5 } = req.query;
+//     const startIndex = (page - 1) * limit;
+//     const qCategory = req.query.category;
+//     const qsort = req.query.sort;
+//     const qColor = req.query.color;
+//     const qSize = req.query.size;
+//     const qs = req.query.s;
+  
+//     try {
+//       let query = Product.find()
+  
+//       const filterArr = [];
+//       if (qs) filterArr.push({
+//         $or: [
+//           { "title": { $regex: qs, $options: "i" } },
+//           { "productno": { $regex: qs, $options: "i" } },
+//           { "desc": { $regex: qs, $options: "i" } },
+//           { "categories": { $in: [qs] } }
+//         ]
+//       })
+  
+//       if (qCategory) filterArr.push({ categories: { $in: [qCategory] } });
+//       if (qColor) filterArr.push({ color: { $in: [qColor] } });
+//       if (qSize) filterArr.push({ size: { $in: [qSize] } });
+//       if (filterArr.length !== 0) {
+//         query = query.find({ $and: filterArr });
+//       }
+  
+//       if (qsort === "Newest") {
+//         query.sort({ createdAt: -1 })
+//       } else if (qsort === "price-asc") {
+//         query.sort({ price: 1 })
+//       } else if (qsort === "price-desc") {
+//         query.sort({ price: -1 })
+//       } else if (qsort === "toppurchased") {
+//         query.sort({ purchasedCount: -1 })
+//       } else if (qsort === "topRated") {
+//         query.sort({ ratingsAverage: -1, ratingsQuantity: -1 })
+//       } else if (qsort === "topreviewed") {
+//         query.sort({ ratingsQuantity: -1 })
+//       }
+//       query.skip(startIndex).limit(limit)
+  
+//       const products = await query.exec()
+  
+//       if (products.length < 1) return res.status(404).json({ message: "No more product Found!" });
+  
+//       res.status(200).json(products);
+  
+  
+//     } catch (error) {
+//       res.status(500).json({ message: "failed to get Product" });
+//     }
+  
+//   });
